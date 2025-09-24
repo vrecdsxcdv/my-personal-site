cf3e877b6c44fc8c0e8cbe8a5920fd6a3e635f8d
Matvey <matvey@MacBook-Pro-Matvey.local>
2025-09-25 00:23:25 +0300

    chore(pages): minimal working GH Pages workflow + correct base

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/my-personal-site/',
-  test: {
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
-    globals: true,
-  },
 })
